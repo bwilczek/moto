2f367d6b7558022264616030401db124bdf0087b
Radoslaw Sporny <r.sporny@gmail.com>
2016-01-04 16:03:30 +0100

    refactor: formatting

--- lib/thread_context.rb
+++ lib/thread_context.rb
@@ -76,51 +76,51 @@ module Moto
     end
 
     def run
-        # remove log files from previous execution
-        Dir.glob("#{@test.dir}/*.log").each {|f| File.delete f }
-        max_attempts = @runner.my_config[:max_attempts] || 1
-        @runner.environments.each do |env|
-          params_path = "#{@test.dir}/#{@test.filename}.yml"
-          params_all = [{}]
-          params_all = YAML.load(ERB.new(File.read(params_path)).result) if File.exists?(params_path)
-          #params_all = YAML.load_file(params_path) if File.exists?(params_path)
-          # or convert keys to symbols?
-          # params_all = YAML.load_file(params_path).map{|h| Hash[ h.map{|k,v| [ k.to_sym, v ] } ] } if File.exists?(params_path)
-          params_all.each_with_index do |params, params_index|
-            # TODO: add filtering out params that are specific to certain envs
-            (1..max_attempts).each do |attempt|
-              @test.init(env, params, params_index)
-              # TODO: log path might be specified (to some extent) by the configuration
-              @test.log_path = "#{@test.dir}/#{@test.name.gsub(/\s+/, '_').gsub('::', '_').gsub('/', '_')}.log"
-              @logger = Logger.new(File.open(@test.log_path, File::WRONLY | File::TRUNC | File::CREAT))
-              # TODO: make logger level configurable
-              @logger.level = @runner.my_config[:log_level]
-              @current_test = @test
-              @runner.listeners.each { |l| l.start_test(@test) }
-              @clients.each_value { |c| c.start_test(@test) }
-              @test.before
-              @logger.info "Start: #{@test.name} attempt #{attempt}/#{max_attempts}"
-              begin
-                @test.run
-              rescue Exceptions::TestForcedPassed, Exceptions::TestForcedFailure, Exceptions::TestSkipped => e
-                logger.info(e.message)
-                @runner.result.add_error(@test, e)
-              rescue Exception => e
-                @logger.error("#{e.class.name}: #{e.message}")
-                @logger.error(e.backtrace.join("\n"))
-                @runner.result.add_error(@test, e)
-              end
-              @test.after
-              @clients.each_value { |c| c.end_test(@test) }
-              # HAX: running end_test on results now, on other listeners after logger is closed
-              @runner.listeners.first.end_test(@test)
-              @logger.info("Result: #{@test.result}")
-              @logger.close
-              @runner.listeners[1..-1].each { |l| l.end_test(@test) }
-              break unless [Result::FAILURE, Result::ERROR].include? @test.result
-            end # RETRY
-          end
+      # remove log files from previous execution
+      Dir.glob("#{@test.dir}/*.log").each {|f| File.delete f }
+      max_attempts = @runner.my_config[:max_attempts] || 1
+      @runner.environments.each do |env|
+        params_path = "#{@test.dir}/#{@test.filename}.yml"
+        params_all = [{}]
+        params_all = YAML.load(ERB.new(File.read(params_path)).result) if File.exists?(params_path)
+        #params_all = YAML.load_file(params_path) if File.exists?(params_path)
+        # or convert keys to symbols?
+        # params_all = YAML.load_file(params_path).map{|h| Hash[ h.map{|k,v| [ k.to_sym, v ] } ] } if File.exists?(params_path)
+        params_all.each_with_index do |params, params_index|
+          # TODO: add filtering out params that are specific to certain envs
+          (1..max_attempts).each do |attempt|
+            @test.init(env, params, params_index)
+            # TODO: log path might be specified (to some extent) by the configuration
+            @test.log_path = "#{@test.dir}/#{@test.name.gsub(/\s+/, '_').gsub('::', '_').gsub('/', '_')}.log"
+            @logger = Logger.new(File.open(@test.log_path, File::WRONLY | File::TRUNC | File::CREAT))
+            # TODO: make logger level configurable
+            @logger.level = @runner.my_config[:log_level]
+            @current_test = @test
+            @runner.listeners.each { |l| l.start_test(@test) }
+            @clients.each_value { |c| c.start_test(@test) }
+            @test.before
+            @logger.info "Start: #{@test.name} attempt #{attempt}/#{max_attempts}"
+            begin
+              @test.run
+            rescue Exceptions::TestForcedPassed, Exceptions::TestForcedFailure, Exceptions::TestSkipped => e
+              logger.info(e.message)
+              @runner.result.add_error(@test, e)
+            rescue Exception => e
+              @logger.error("#{e.class.name}: #{e.message}")
+              @logger.error(e.backtrace.join("\n"))
+              @runner.result.add_error(@test, e)
+            end
+            @test.after
+            @clients.each_value { |c| c.end_test(@test) }
+            # HAX: running end_test on results now, on other listeners after logger is closed
+            @runner.listeners.first.end_test(@test)
+            @logger.info("Result: #{@test.result}")
+            @logger.close
+            @runner.listeners[1..-1].each { |l| l.end_test(@test) }
+            break unless [Result::FAILURE, Result::ERROR].include? @test.result
+          end # RETRY
         end
+      end
       @clients.each_value { |c| c.end_run }
     end
 
