69965c73dc5cbd25bcb60d5391d72c388e041c07
Michal Kujawski <michal.kujawski@proofhq.com>
2016-04-19 11:33:17 +0200

    README.md - updated a bit forward_context_methods - commented using text from README lesson03_test_parameters.yml - changed to Ruby instead of YML and removed extension

--- lib/forward_context_methods.rb
+++ lib/forward_context_methods.rb
@@ -1,18 +1,21 @@
 module Moto
   module ForwardContextMethods
-
+      # Access client object instance for given class name.
       def client(name)
         @context.client(name)
       end
-      
+
+      # Write message to test execution log file. See Ruby Logger class for details.
       def logger
         @context.logger
       end
-      
+
+      # Read const value specific for current environment from `config/const.yml` file
       def const(key)
         @context.const(key)
       end
-      
+
+      # Access currently executed test
       def test
         @context.test
       end
