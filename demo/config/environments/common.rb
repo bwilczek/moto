faff36d2d5d1b975513d0afa1003e7ab1b3af5b2
Michal Kujawski <michal.kujawski@proofhq.com>
2016-05-18 21:28:46 +0200

    updated exemplary configurations (env constants)

---
+++ demo/config/environments/common.rb
@@ -0,0 +1,13 @@
+{
+  section_name1:
+  {
+    key1: 'This value can be accessed by Moto::Lib::Config.environment_const(section_name1.key1)',
+    key2: 'common value 2'
+  },
+
+  section_name2:
+  {
+    key1: 'common value 3',
+    key2: 'common value 4'
+  }
+}
---
+++ demo/config/environments/exemplary_env.rb
@@ -0,0 +1,7 @@
+{
+  section_name1:
+  {
+    key2:             'overriden common value',
+    env_specific_key: 'This value can be accessed by Moto::Lib::Config.environment_cons(section_name1.env_specific_key)'
+  }
+}
