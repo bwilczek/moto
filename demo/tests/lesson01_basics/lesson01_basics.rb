e8603c5d22ce61ba22356efa48fe368a609032e0
Michal Kujawski <michal.kujawski@proofhq.com>
2016-09-05 13:50:27 +0200

    removed BASE CODE tests from Moto - only full code ones are still supported

--- demo/tests/lesson01_basics/lesson01_basics.rb
+++ demo/tests/lesson01_basics/lesson01_basics.rb
@@ -1,9 +1,21 @@
 # Line below shows how to tag tests
+
 # MOTO_TAGS: demo, regression
 
-# pass("I like it.")            # instant finish as PASSED
-# fail("I don't like it.")      # instant finish as FAILURE
-# skip("Jump, jump!")           # instant finish as SKIPPED
-# raise("Something is wrong!")  # instant finish as ERROR
+module MotoApp
+  module Tests
+    class Lesson09FullCode < Moto::Test::Base
 
-assert_equal(2+2, 4)
+      def run
+        assert_equal(2+2, 4)
+
+        # pass("I like it.")            # instant finish as PASSED
+        # fail("I don't like it.")      # instant finish as FAILURE
+        # skip("Jump, jump!")           # instant finish as SKIPPED
+        # raise("Something is wrong!")  # instant finish as ERROR
+      end
+
+    end
+  end
+end
+
--- demo/tests/nested/lesson01_basics/lesson01_basics.rb
+++
@@ -1,9 +0,0 @@
-# Line below shows how to tag tests
-# MOTO_TAGS: demo, regression
-
-# pass("I like it.")            # instant finish as PASSED
-# fail("I don't like it.")      # instant finish as FAILURE
-# skip("Jump, jump!")           # instant finish as SKIPPED
-# raise("Something is wrong!")  # instant finish as ERROR
-
-assert_equal(2+2, 4)
