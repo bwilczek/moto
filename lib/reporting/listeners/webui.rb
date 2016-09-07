3b5a1da83771bf34bebaabed98e8f3a24b2a5b96
Michal Kujawski <michal.kujawski@proofhq.com>
2016-09-07 13:59:16 +0200

    webui now supports timeouts and max retries for rest connections

--- lib/reporting/listeners/webui.rb
+++ lib/reporting/listeners/webui.rb
@@ -6,74 +6,105 @@ module Moto
     module Listeners
       class Webui < Base
 
+        REST_MAX_TRIES = 3
+        REST_TIMEOUT = 15
+
         def start_run
 
           @url = Moto::Lib::Config.moto[:test_reporter][:listeners][:webui][:url]
 
           data = {
-            name:         custom_run_name,
-            result:       :running,
-            cnt_all:      nil,
-            cnt_passed:   nil,
-            cnt_failure:  nil,
-            cnt_error:    nil,
-            cnt_skipped:  nil,
-            user:         Sys::Uname.sysname.downcase.include?('windows') ? ENV['USERNAME'] : ENV['LOGNAME'],
-            host:         Sys::Uname.nodename,
-            pid:          Process.pid
+              name: custom_run_name,
+              result: :running,
+              cnt_all: nil,
+              cnt_passed: nil,
+              cnt_failure: nil,
+              cnt_error: nil,
+              cnt_skipped: nil,
+              user: Sys::Uname.sysname.downcase.include?('windows') ? ENV['USERNAME'] : ENV['LOGNAME'],
+              host: Sys::Uname.nodename,
+              pid: Process.pid
           }
 
-          @run = JSON.parse( RestClient.post( "#{@url}/api/runs", data.to_json, :content_type => :json, :accept => :json ) )
+          result = try {
+            RestClient::Request.execute(method: :post, url: "#{@url}/api/runs", payload: data.to_json, timeout: REST_TIMEOUT, headers: {content_type: :json, accept: :json})
+          }
+
+          @run = JSON.parse(result)
           @tests = {}
         end
 
         def end_run(run_status)
           # PUT http://sandbox.dev:3000/api/runs/1
           data = {
-            result:       run_status.result,
-            cnt_all:      run_status.tests_all.length,
-            cnt_passed:   run_status.tests_passed.length,
-            cnt_failure:  run_status.tests_failed.length,
-            cnt_error:    run_status.tests_error.length,
-            cnt_skipped:  run_status.tests_skipped.length,
-            duration:     run_status.duration
+              result: run_status.result,
+              cnt_all: run_status.tests_all.length,
+              cnt_passed: run_status.tests_passed.length,
+              cnt_failure: run_status.tests_failed.length,
+              cnt_error: run_status.tests_error.length,
+              cnt_skipped: run_status.tests_skipped.length,
+              duration: run_status.duration
           }
 
-          @run = JSON.parse( RestClient.put( "#{@url}/api/runs/#{@run['id']}", data.to_json, :content_type => :json, :accept => :json ) )
+          result = try {
+              RestClient::Request.execute(method: :put, url: "#{@url}/api/runs/#{@run['id']}", payload: data.to_json, timeout: REST_TIMEOUT, headers: {content_type: :json, accept: :json})
+          }
+          @run = JSON.parse(result)
         end
 
         def start_test(test_status)
           # POST http://sandbox.dev:3000/api/tests/create
           data = {
-            name:       test_status.name,
-            class_name: test_status.test_class_name,
-            log:        nil,
-            run_id:     @run['id'],
-            env:        test_status.env,
-            parameters: test_status.params.to_s,
-            result:     :running,
-            error:      nil,
-            failures:   nil
+              name: test_status.name,
+              class_name: test_status.test_class_name,
+              log: nil,
+              run_id: @run['id'],
+              env: test_status.env,
+              parameters: test_status.params.to_s,
+              result: :running,
+              error: nil,
+              failures: nil
           }
 
-          @tests[test_status.name] = JSON.parse( RestClient.post( "#{@url}/api/tests", data.to_json, :content_type => :json, :accept => :json ) )
+          result = try {
+            RestClient::Request.execute(method: :post, url: "#{@url}/api/tests", payload: data.to_json, timeout: REST_TIMEOUT, headers: {content_type: :json, accept: :json})
+          }
+          @tests[test_status.name] = JSON.parse(result)
         end
 
         def end_test(test_status)
           data = {
-            log:      File.read(test_status.log_path),
-            result:   test_status.results.last.code,
-            error:    test_status.results.last.code == Moto::Test::Result::ERROR ? nil : test_status.results.last.message,
-            failures: test_failures(test_status),
-            duration: test_status.duration
+              log: File.read(test_status.log_path),
+              result: test_status.results.last.code,
+              error: test_status.results.last.code == Moto::Test::Result::ERROR ? nil : test_status.results.last.message,
+              failures: test_failures(test_status),
+              duration: test_status.duration
           }
 
-          @tests[test_status.name] = JSON.parse( RestClient.put( "#{@url}/api/tests/#{@tests[test_status.name]['id']}", data.to_json, :content_type => :json, :accept => :json ) )
+          result = try {
+            RestClient::Request.execute(method: :put, url: "#{@url}/api/tests/#{@tests[test_status.name]['id']}", payload: data.to_json, timeout: REST_TIMEOUT, headers: {content_type: :json, accept: :json})
+          }
+          @tests[test_status.name] = JSON.parse(result)
         end
 
         # @return [String] string with messages of all failures in a test
         def test_failures(test_status)
           test_status.results.last.failures.join("\n\t")
+        end
+
+        # Tries to execute, without an error, block of code passed to the function.
+        # @param block Block of code to be executed up to MAX_REST_TRIES
+        def try(&block)
+
+          tries = REST_MAX_TRIES
+
+          begin
+            yield
+          rescue
+            tries -= 1
+            tries > 0 ? retry : raise
+          end
+
         end
 
       end
